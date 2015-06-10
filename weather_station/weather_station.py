f919b45e9ea06277117d3a0a8e75662249228593
Maciej Szankin <maciej.szankin@intel.com>
2015-06-10 13:44:53 +0200

    Changed random location to match Poland territory. Changed temp and humidity to be around expected values. Changed logging messages.

--- weather_station/weather_station.py
+++ weather_station/weather_station.py
@@ -8,7 +8,7 @@ from db.create_db import session
 from datetime import datetime
 
 
-LOG_FORMAT = ('%(levelname) -5s %(asctime)s %(name) -10s %(funcName) -5s %(lineno) -5d: %(message)s')
+LOG_FORMAT = ('%(levelname) -5s %(asctime)s %(name) -5s %(funcName) -5s %(lineno) -5d: %(message)s')
 LOG_LEVEL = logging.DEBUG
 logging.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL)
 
@@ -17,16 +17,20 @@ class WeatherStation(object):
 
     def __init__(self):
         self.interval = 5
+        self.base_temperature = random.uniform(15.0, 20.0)
+        self.base_humidity = random.uniform(45.0, 75.0)
         self.model = WeatherStationModel()
         self.model.name = 'ws_%s' % socket.gethostname()
         self.model.id = str(uuid.uuid5(uuid.NAMESPACE_DNS, self.model.name))
         if not self._exists():
-            logging.debug('WS does not exists. Creating new.')
             self.model.deleted = 0
             self.model.is_sent = 0
-            self.model.latitude = random.uniform(-90.0, 90.0)
-            self.model.longitude = random.uniform(-90.0, 90.0)
+            self.model.latitude = float("{0:.4f}".format(random.uniform(51.0, 54.0)))
+            self.model.longitude = float("{0:.4f}".format(random.uniform(15.0, 25.0)))
             self.model.metric_types = self._available_metric_types()
+            logging.debug('WS does not exists. Creating new: %s @ (%s N, %s E)' % (self.model.name,
+                                                                                   self.model.latitude,
+                                                                                   self.model.longitude))
             session.merge(self.model)
             session.commit()
         else:
@@ -54,21 +58,27 @@ class WeatherStation(object):
         return session.query(MetricTypeModel).all()
 
     def _generate_metrics_data(self):
-        amount = 0
+        logging.info('Added new metrics [%s]:' % len(self.model.metric_types))
         for metric_type in self.model.metric_types:
             new_metric = MetricModel()
             new_metric.metric_type = metric_type
             new_metric.metric_type_id = metric_type.id
             new_metric.id = str(uuid.uuid4())
             new_metric.is_sent = 0
-            new_metric.value = random.uniform(metric_type.min_value, metric_type.max_value)
+            if metric_type.name == 'Temperature':
+                new_metric.value = float("{0:.2f}".format(random.uniform(self.base_temperature-5,
+                                                                         self.base_temperature+5)))
+            elif metric_type.name == 'Humidity':
+                new_metric.value = float("{0:.2f}".format(random.uniform(self.base_humidity-10,
+                                                                         self.base_humidity+10)))
+            else:
+                new_metric.value = random.uniform(metric_type.min_value, metric_type.max_value)
             new_metric.weather_station = self.model
             new_metric.weather_station_id = self.model.id
             new_metric.timestamp = datetime.utcnow().replace(microsecond=0)
             session.add(new_metric)
-            amount += 1
+            logging.debug('\t%s :\t%s %s' % (str(metric_type.name), new_metric.value, metric_type.unit))
         session.commit()
-        logging.info('Added new metrics [%s]' % str(amount))
 
 if __name__ == '__main__':
     ws = WeatherStation()
